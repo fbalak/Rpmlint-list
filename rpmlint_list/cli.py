bb50e38cf113bd3225de11fd88efc120478672c6
Filip Balák <fbalak@redhat.com>
2017-08-15 04:43:29 -0400

    add --html cli option, add generate_html_content function

--- rpmlint_list/cli.py
+++ rpmlint_list/cli.py
@@ -3,15 +3,20 @@
 """Console script for rpmlint_list."""
 
 import click
+import json
 import rpmlint_list
 
 @click.command()
+@click.option('--html', '-h', is_flag=True, help='Generates html output for provided XML.')
 @click.argument('url')
-def main(url):
+def main(html, url):
     """Creates reverse index list from provided URL with XML"""
     error_list = rpmlint_list.get_error_list(url)
-    error_json = rpmlint_list.get_error_json(error_list)
-    click.echo(error_json)
+    error_dictionary = rpmlint_list.get_error_dictionary(error_list)
+    if html:
+        click.echo(rpmlint_list.generate_html_content(error_dictionary))
+    else:
+        click.echo(json.dumps(error_dictionary))
 
 
 if __name__ == "__main__":
--- rpmlint_list/rpmlint_list.py
+++ rpmlint_list/rpmlint_list.py
@@ -1,7 +1,6 @@
 import re
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
-import json
 
 
 def get_error_list(url):
@@ -15,10 +14,20 @@ def get_error_list(url):
             error_list += pattern.findall(failure.text)
     return error_list
 
-def get_error_json(error_list):
+def get_error_dictionary(error_list):
     error_dictionary = {}
     for error in error_list:
         if error[1] not in error_dictionary:
             error_dictionary[error[1]] = []
         error_dictionary[error[1]].append(error[0])
-    return json.dumps(error_dictionary)
+    return error_dictionary
+
+def generate_html_content(error_json):
+    ul = "<ul><li>"
+    for error in error_json:
+        ul += "</li><li>".join(error_json[error])
+    ul +="</li></ul>"
+    content = """<div>
+    {}
+    </div>""".format(ul)
+    return content
